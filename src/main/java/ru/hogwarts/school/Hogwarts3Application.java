ac7f965acba918f3d071dc0b46d10e007ebe3d06
anna-ermakova <korovanna8@mail.ru>
2023-06-29 17:36:42 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Hogwarts3Application.java
@@ -0,0 +1,10 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogwarts3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hogwarts3Application.class, args
---
+++ src/test/java/ru/hogwarts/school/Hogwarts3ApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogwarts3ApplicationTests {
+
+    @Test
+    void contextLoads() {
